de0cc404052e562c3cf9bccf64acfdb1529f218a
johnberdin <john.berdin@stud.fh-campuswien.ac.at>
2021-03-11 21:59:45 +0100

    checkPassword_null resolved

--- src/main/java/Password.java
+++ src/main/java/Password.java
@@ -5,37 +5,39 @@ public class Password {
 
 
     public static Boolean checkPassword(String password){
-        if(password.length()<8 || password.length() > 25){
-            return false;
-        }
-        char helpChar;
-        int lowerCaseCounter = 0;
-        int upperCaseCounter = 0;
-        int numberCounter = 0;
-        int specialCharCounter = 0;
-        char[] specSymbols = {'(',')','$','?','!','%','/','@'};
-
-        for(int i = 0; i < password.length(); i++){
-            helpChar = password.charAt(i);
-            if(Character.isLowerCase(helpChar)){
-                lowerCaseCounter++;
-            }else if(Character.isUpperCase(helpChar)){
-                upperCaseCounter++;
-            }else if(Character.isDigit(helpChar)){
-                numberCounter++;
+        if(password != null) {
+            if (password.length() < 8 || password.length() > 25) {
+                return false;
             }
-            else {
-                for(int j = 0; j < specSymbols.length; j++) {
-                    if (helpChar ==  specSymbols[j])
-                        specialCharCounter++;
-                    else
-                        return false;
+            char helpChar;
+            int lowerCaseCounter = 0;
+            int upperCaseCounter = 0;
+            int numberCounter = 0;
+            int specialCharCounter = 0;
+            char[] specSymbols = {'(', ')', '$', '?', '!', '%', '/', '@'};
+
+            for (int i = 0; i < password.length(); i++) {
+                helpChar = password.charAt(i);
+                if (Character.isLowerCase(helpChar)) {
+                    lowerCaseCounter++;
+                } else if (Character.isUpperCase(helpChar)) {
+                    upperCaseCounter++;
+                } else if (Character.isDigit(helpChar)) {
+                    numberCounter++;
+                } else {
+                    for (int j = 0; j < specSymbols.length; j++) {
+                        if (helpChar == specSymbols[j])
+                            specialCharCounter++;
+                        else
+                            return false;
+                    }
                 }
             }
+            if (lowerCaseCounter == 0 || upperCaseCounter == 0 || numberCounter == 0 || specialCharCounter == 0) {
+                return false;
+            }
+            return true;
         }
-        if(lowerCaseCounter == 0 || upperCaseCounter == 0 || numberCounter == 0 || specialCharCounter == 0) {
-            return false;
-        }
-        return true;
+        return false;
     }
 }
